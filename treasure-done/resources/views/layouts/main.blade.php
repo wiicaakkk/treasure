d441383688d48d48f22a8a5658b01d654fde05de
wiicaakkk <bagus.wicaksono285@gmail.com>
2022-04-10 22:35:28 +0700

    ngantuk

---
+++ treasure-done/resources/views/layouts/main.blade.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" href="favicon.svg" type="image/png">
+    <link rel="stylesheet" href="css/flexslider.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/font-icon.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style4.css" />
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/animate.css/3.5.1/animate.min.css">
+    <title>Treasure</title>
+  </head>
+  <body>
+  <!-- header section -->
+    <section class="banner" role="banner" id="home">
+        <header id="header">
+        <div class="header-content clearfix">
+            <nav class="navigation" role="navigation">
+            <ul class="primary-nav">
+            <li><a href="#home" style="text-decoration:none">Beranda</a></li>
+                <li><a href="#intro" style="text-decoration:none">Tentang Treasure</a></li>
+            <li><a href="#teams" style="text-decoration:none">Peta Wisata</a></li>  
+                <li><a href="#gallery" style="text-decoration:none">Wisata Terbaru</a></li>
+            </ul>
+            </nav>
+            <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+        </header>
+        <!-- banner text --> 
+    
+    <div id="first-slider">
+        <div id="carousel-example-generic" class="carousel slide carousel-fade">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <!-- Item 1 -->
+                <div class="item active slide1">
+                    <div class="row"><div class="container"> 
+                        <div class="col-md-9 text-left">
+                            <h3>Treasure</h3>
+                            <h4>Web Tugas Besar Aplikasi Berbasis Platfrom</h4>             
+                        </div>
+                    </div>
+                </div>     
+            </div>
+        </div>
+    </div>
+    </section>
+    <!-- header section --> 
+    
+    <!-- intro section -->
+    <section id="intro" class="section intro">
+        <div class="container">
+        <div class="section-header">
+            <h2 class="">Tentang Treasure</h2>
+        </div>
+        <div class="row">  
+            <div id="content24" data-section="content-24" class="data-section"> 
+        <div class="">
+        <center><p>Aplikasi ‘Treasure’ ini ditujukan untuk wisatawan yang ingin mencari informasi tempat wisata suatu daerah yang berada di Kota Bandung. diharapkan bisa mempermudah masyarakat setempat ataupun wisatawan yang ingin mengetahui lebih detail tentang wisata apa saja yang dapat dikunjunginya. Selain itu, ‘Treasure’ juga menyediakan informasi akses ke lokasi dengan mudah, foto/video tempat wisata, keterangan fasilitas yang dapat dinikmati oleh wisatawan, dan beberapa informasi lainnya terkait tujuan pengembangan lokawisata bisa lebih dikenal banyak wisatawan domestik maupun mancanegara. 
+        </p></center>
+        </div>
+    </section>
+    <!-- intro section --> 
+    
+    
+    <!-- our team section -->
+    <section id="teams" class="section teams">
+        <div class="container">
+            <div class="section-header">
+                    <h2 class="">Peta Wisata</h2><br>
+                        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+            <div class="person"><img src="image/island.png" " alt="" class="rounded img-responsive">
+                <div class="person-content">
+                <h4>Objek Wisata</h4> 
+                <p>Menyediakan informasi obyek wisata di Bandung</p>
+                <a href="/maps" style="text-decoration:none"" class="morebtn">Explore Map</a>
+                </div>
+            </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+            <div class="person"> <img src="image/food.png" alt="" class="rounded  img-responsive">
+                <div class="person-content">
+                <h4>Kuliner</h4> 
+                <p>Menyediakan informasi mengenai wisata kuliner yang ada di Bandung</p>
+                <a href="#"" style="text-decoration:none" class="morebtn">Explore Map</a>
+                </div> 
+            </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+            <div class="person"> <img src="image/card.png" " alt="" class="rounded  img-responsive">
+                <div class="person-content">
+                <h4>Belanja</h4> 
+                <p>Menyediakan informasi mengenai tempat belanja yang ada di Bandung</p>
+                <a href="#" style="text-decoration:none" class="morebtn">Explore Map</a>
+                </div> 
+            </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+            <div class="person"> <img src="image/hotel.png" " alt="" class="rounded  img-responsive">
+                <div class="person-content">
+                <h4>Hotel</h4> 
+                <p>Menyediakan informasi rekomendasi hotel pilihan yang ada di Bandung</p>
+                <a href="#" style="text-decoration:none" class="morebtn">Explore Map</a>
+                </div> 
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <!-- our team section --> 
+    
+    
+    <!-- gallery section -->
+    <section id="gallery" class="gallery section">
+        <div class="container-fluid">
+        <div class="section-header">
+                <h2 class="">Wisata Terbaru</h2>
+                <p class="">Bandung ini seperti selalu punya tempat baru yang menggoda untuk dijelajahi 
+                dan disaksikan sendiri keindahannya. </br>
+            Berikut rekomendasi destinasi wisata terbaru di Bandung yang lagi hits, kekinian, dan Instragramable buat kamu kunjungi:</p>
+                </div>
+        <div class="row no-gutter">
+            <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="../image/gambar1.PNG" class="work-box"> <img src="../image/gambar1.PNG" alt="">
+            <div class="overlay">
+                <div class="overlay-caption">
+                <p><strong>Obelix Hills</strong></p>
+                </div>
+            </div>
+            <!-- overlay --> 
+            </a> </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="../image/gambar1.PNG" class="work-box"> <img src="../image/gambar1.PNG" alt="">
+            <div class="overlay">
+                <div class="overlay-caption">
+                <p><strong>Illumina Glamping Jogja</strong></p>
+                </div>
+            </div>
+            <!-- overlay --> 
+            </a> </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="../image/gambar1.PNG" class="work-box"> <img src="../image/gambar1.PNG" alt="">
+            <div class="overlay">
+                <div class="overlay-caption">
+                <p><strong>Grojogan Watu Purbo</strong></p>
+                </div>
+            </div>
+            <!-- overlay --> 
+            </a> </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="../image/gambar1.PNG" class="work-box"> <img src="../image/gambar1.PNG" alt="">
+            <div class="overlay">
+                <div class="overlay-caption"> 
+                <p><strong>Teras Merapi</strong></p>
+                </div>
+            </div>
+            <!-- overlay --> 
+            </a> </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="../image/gambar1.PNG" class="work-box"> <img src="../image/gambar1.PNG" alt="">
+            <div class="overlay">
+                <div class="overlay-caption">
+                <p><strong>Lalang Buana - Agrowisata Bhumi Merapi</strong></p>
+                </div>
+            </div>
+            <!-- overlay --> 
+            </a> </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="../image/gambar1.PNG" class="work-box"> <img src="../image/gambar1.PNG" alt="">
+            <div class="overlay">
+                <div class="overlay-caption">
+                <p><strong>Restoe Boemi Kreo</strong></p>
+                </div>
+            </div>
+            <!-- overlay --> 
+            </a> </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="../image/gambar1.PNG" class="work-box"> <img src="../image/gambar1.PNG" alt="">
+            <div class="overlay">
+                <div class="overlay-caption">
+                <p><strong>Floating Resto</strong></p>
+                </div>
+            </div>
+            <!-- overlay --> 
+            </a> </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="../image/gambar1.PNG" class="work-box"> <img src="../image/gambar1.PNG" alt="">
+            <div class="overlay">
+                <div class="overlay-caption">
+                <p><strong>Suwatu by Mil&Bay</strong></p>
+                </div>
+            </div>
+            <!-- overlay --> 
+            </a> </div>
+        </div>
+        </div>
+    </section>
+    <!-- gallery section --> 
+    
+    
+    <footer id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="widget">
+                        <h5 class="widgetheading"><a href="#"><img src="image/treasure.png" alt="logo"></a></
+                        <h5 class="widgetheading"></h5>
+                        <address><br>
+                        <strong>Tugas Besar Aplikasi Berbasis Platfrom, Membuat web bertema Pariwisata</strong><br>
+                    </address>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="widget">
+                    </div>
+                </div>
+            <div class="col-sm-3">
+                    <div class="widget">
+                        <h5 class="widgetheading">Tentang Kami</h5>
+                <ul class="link-list"><br>
+                <li><p>  <strong>Kelompok 08</strong></p></li>
+                <li><p> Bagus Wicaksono Nurjayanto (1301194280)</p></li>
+            </ul>
+                    </div>
+                </div>
+        </div>
+        <div class="footer_bottom"><span>Copyright ©2022 </span> 
+        </div>
+    </footer>  
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+    <script src="js/jquery.flexslider-min.js"></script> 
+    <script src="js/jquery.fancybox.pack.js"></script> 
+    <script src="js/retina.min.js"></script> 
+    <script src="js/modernizr.js"></script> 
+    <script src="js/main.js"></script> 
+    <script type="text/javascript" src="js/jquery.contact.js"></script> 
+  </body>
+</html>
