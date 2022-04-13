29fef2db23b56e06842568f8eba69e730a209c28
KhairatHayati <103499296+KhairatHayati@users.noreply.github.com>
2022-04-13 22:37:50 +0700

    Update register.blade.php

--- treasure-palingakhir/resources/views/auth/register.blade.php
+++ treasure-palingakhir/resources/views/auth/register.blade.php
@@ -1,77 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="row justify-content-center">
+    <div class="col-md-5">
+    <main class="form-register">
+      <form action="/register" method="post">
+        <center><h1 class="h3 mb-3 fw-normal">Form Regristrasi</h1></center>
+        @csrf
+            <div class="form-floating md-5">
+              <input type="text" name="name" class="form-control roude-top  @error('name') is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}">
+              <label for="name">Name</label>
+              @error('name')
+                <div class="invalid-feedback">
+                {{ $message }}
                 </div>
+              @enderror
             </div>
+
+            <div class="form-floating md-5">
+                <input type="text" name="Username" class="form-control   @error('Username') is-invalid @enderror" id="Username" placeholder="Username" required value="{{ old('Username') }}">
+                <label for="username">UserName</label>
+                @error('Username')
+                  <div class="invalid-feedback">
+                  {{ $message }}
+                  </div>
+                @enderror
+              </div>
+
+              <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+                @error('Email address')
+                    <div class="invalid-feedback">
+                    {{ $message }}
+                    </div>
+                @enderror
+              </div>
+
+              <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+                @error('Password')
+                    <div class="invalid-feedback">
+                    {{ $message }}
+                    </div>
+                @enderror
+              </div>
+
+        <div class="mt-3">
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Regristrasi</button>
         </div>
+      </form>
+      <small class="d-block text-center mt-3">Sudah terdaftar? <a href="/login">Login sekarang!</a></small>
+    </main>
     </div>
-</div>
+  </div>
 @endsection
